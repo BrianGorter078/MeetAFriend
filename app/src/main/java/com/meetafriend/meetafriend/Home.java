6dc603f8edc84122134fdfe9861b3f886f14f202
BrianGorter078 <Briangorter@hotmail.com>
2015-05-12 16:38:40 +0200

    lol

--- app/src/main/java/com/meetafriend/meetafriend/Home.java
+++ app/src/main/java/com/meetafriend/meetafriend/Home.java
@@ -1,12 +1,12 @@
 package com.meetafriend.meetafriend;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Home extends ActionBarActivity {
+public class Home extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
