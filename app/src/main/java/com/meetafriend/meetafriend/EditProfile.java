a754e97a4d4ee6a31ea1dda95da14519d1016363
BrianGorter078 <Briangorter@hotmail.com>
2015-05-18 14:27:39 +0200

    Activity

--- app/src/main/java/com/meetafriend/meetafriend/EditProfile.java
+++ app/src/main/java/com/meetafriend/meetafriend/EditProfile.java
@@ -1,12 +1,13 @@
 package com.meetafriend.meetafriend;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class EditProfile extends ActionBarActivity {
+public class EditProfile extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/meetafriend/meetafriend/Search.java
+++ app/src/main/java/com/meetafriend/meetafriend/Search.java
@@ -1,12 +1,13 @@
 package com.meetafriend.meetafriend;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Search extends ActionBarActivity {
+public class Search extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
