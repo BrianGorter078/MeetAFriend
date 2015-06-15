7b5058a7e3ff8def35b3ce38df694a9a0576a592
Kevin Boer <kevin@boermedia.com>
2015-06-15 13:01:52 +0200

    update

--- app/src/main/java/com/meetafriend/meetafriend/Register.java
+++ app/src/main/java/com/meetafriend/meetafriend/Register.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +39,10 @@ public class Register extends Activity implements View.OnClickListener {
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
     private String gender;
+    private String gender_want;
+    RadioGroup rg;
+    int pos;
+    int pos1;
 
 
     @Override
@@ -48,7 +53,7 @@ public class Register extends Activity implements View.OnClickListener {
         pass = (EditText) findViewById(R.id.passwordRegister);
         email = (EditText) findViewById(R.id.emailRegister);
         age = (EditText) findViewById(R.id.ageRegister);
-        //gender = (EditText) findViewById(R.id.passwordRegister);
+        //gender = (RadioButton) findViewById(R.id.youAreRadiobuttonFemale);
         bLogin = (Button) findViewById(R.id.makeAccountButton);
         bLogin.setOnClickListener(this);
 
@@ -58,6 +63,52 @@ public class Register extends Activity implements View.OnClickListener {
 //        toolbarFriends.setVisibility(View.INVISIBLE);
 //        ImageButton toolbarSettings = (ImageButton) findViewById(R.id.toolbarSettings);
 //        toolbarSettings.setVisibility(View.INVISIBLE);
+
+
+        rg = (RadioGroup) findViewById(R.id.radio_gender);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // TODO Auto-generated method stub
+
+                // Method 1 For Getting Index of RadioButton
+                pos=rg.indexOfChild(findViewById(checkedId));
+
+                Toast.makeText(getBaseContext(), "Method 1 ID = "+String.valueOf(pos),
+                        Toast.LENGTH_SHORT).show();
+
+                //Method 2 For Getting Index of RadioButton
+                pos1=rg.indexOfChild(findViewById(rg.getCheckedRadioButtonId()));
+
+                Toast.makeText(getBaseContext(), "Method 2 ID = "+String.valueOf(pos1),
+                        Toast.LENGTH_SHORT).show();
+
+                switch (pos)
+                {
+                    case 0 :
+                        Toast.makeText(getBaseContext(), "You have Clicked RadioButton 1",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1 :
+                        Toast.makeText(getBaseContext(), "You have Clicked RadioButton 2",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2 :
+                        Toast.makeText(getBaseContext(), "You have Clicked RadioButton 3",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    default :
+                        //The default selection is RadioButton 1
+                        Toast.makeText(getBaseContext(),"You have Clicked RadioButton 1" ,
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+
     }
     public void onClick(View v) {
         new AttemptRegister().execute(); // here we have used, switch case, because on login activity you may //also want to show registration button, so if the user is new ! we can go the //registration activity , other than this we could also do this without switch //case.
@@ -85,7 +136,9 @@ public class Register extends Activity implements View.OnClickListener {
             String register_username = user.getText().toString();
             String register_password = pass.getText().toString();
             String register_emailaddress = email.getText().toString();
-            String register_age = age.getText().toString();
+            String register_age = "0"; //String register_age = age.getText().toString();
+            //String register_gender = gender.toString();
+            //System.out.println(register_gender);
 
             if(user.length()!= 0){
                 try {
@@ -94,6 +147,8 @@ public class Register extends Activity implements View.OnClickListener {
                     params.add(new BasicNameValuePair("password", register_password));
                     params.add(new BasicNameValuePair("email", register_emailaddress));
                     params.add(new BasicNameValuePair("age", register_age));
+                    //params.add(new BasicNameValuePair("gender", register_gender));
+
                     //params.add(new BasicNameValuePair("gender", gender));
                     //Log.d("request!", "starting");
                     JSONObject json = jsonParser.makeHttpRequest(REGISTER_URL, "POST", params); // checking log for json response
@@ -149,8 +204,8 @@ public class Register extends Activity implements View.OnClickListener {
     }
 
     public void checkbuttonsFemale(View view) {
-        RadioButton female = (RadioButton) findViewById(R.id.youAreRadiobuttonFemale);
-        RadioButton male = (RadioButton) findViewById(R.id.youAreRadiobuttonMale);
+        RadioButton female = (RadioButton) findViewById(R.id.gender1);
+        RadioButton male = (RadioButton) findViewById(R.id.gender0);
         RadioButton male2 = (RadioButton) findViewById(R.id.youWantRadiobuttonMale);
         RadioButton female2 = (RadioButton) findViewById(R.id.youWantRadiobuttonFemale);
 
@@ -158,7 +213,7 @@ public class Register extends Activity implements View.OnClickListener {
             gender = "F";
             male.setChecked(false);
             female.setChecked(true);
-            System.out.println(gender);
+            System.out.println(gender.toString());
         }
         else if(!female.isChecked()) {
             male.setChecked(true);
@@ -169,17 +224,18 @@ public class Register extends Activity implements View.OnClickListener {
             gender = "F";
             male2.setChecked(false);
             female2.setChecked(true);
-            System.out.println(gender);
+            System.out.println(gender.toString());
         }
         else if(!female2.isChecked()) {
+            gender_want = "M";
             male2.setChecked(true);
             female2.setChecked(false);
         }
     }
 
     public void checkbuttonsMale(View view) {
-        RadioButton male = (RadioButton) findViewById(R.id.youAreRadiobuttonMale);
-        RadioButton female = (RadioButton) findViewById(R.id.youAreRadiobuttonFemale);
+        RadioButton male = (RadioButton) findViewById(R.id.gender0);
+        RadioButton female = (RadioButton) findViewById(R.id.gender1);
         RadioButton male2 = (RadioButton) findViewById(R.id.youWantRadiobuttonMale);
         RadioButton female2 = (RadioButton) findViewById(R.id.youWantRadiobuttonFemale);
 
@@ -187,7 +243,7 @@ public class Register extends Activity implements View.OnClickListener {
             gender = "M";
             male.setChecked(true);
             female.setChecked(false);
-            System.out.println(gender);
+            System.out.println(gender.toString());
         }
         else if(!male.isChecked()) {
             male.setChecked(false);
@@ -195,12 +251,13 @@ public class Register extends Activity implements View.OnClickListener {
         }
 
         if(male2.isChecked()) {
-            gender = "M";
+            gender_want = "M";
             male2.setChecked(true);
             female2.setChecked(false);
-            System.out.println(gender);
+            System.out.println(gender.toString());
         }
         else if(!male2.isChecked()) {
+            gender_want = "F";
             male2.setChecked(false);
             female2.setChecked(true);
         }
